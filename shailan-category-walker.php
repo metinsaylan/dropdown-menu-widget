9754406093101db3c98fcea0fb21b6a0f0547041
Metin Şaylan <metinsaylan@gmail.com>
2017-10-06 15:14:40 +0300

    remove comments

--- shailan-category-walker.php
+++ shailan-category-walker.php
@@ -1,36 +1,10 @@
-<?php // is the love
+<?php
 
-/**
- * Create HTML list of categories.
- *
- * @package Shailan Dropdown Menu Widget
- * @since 2.1.0
- * @uses Walker
- */
 class shailan_CategoryWalker extends Walker {
-	/**
-	 * @see Walker::$tree_type
-	 * @since 2.1.0
-	 * @var string
-	 */
-	var $tree_type = 'category';
 
-	/**
-	 * @see Walker::$db_fields
-	 * @since 2.1.0
-	 * @todo Decouple this
-	 * @var array
-	 */
+	var $tree_type = 'category';
 	var $db_fields = array ('parent' => 'parent', 'id' => 'term_id');
 
-	/**
-	 * @see Walker::start_lvl()
-	 * @since 2.1.0
-	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @param int $depth Depth of category. Used for tab indentation.
-	 * @param array $args Will only append content if style argument value is 'list'.
-	 */
 	function start_lvl( &$output, $depth = 0, $args = array() ) {
 		if ( 'list' != $args['style'] )
 			return;
@@ -39,14 +13,6 @@ class shailan_CategoryWalker extends Walker {
 		$output .= "$indent<ul class='children'>\n";
 	}
 
-	/**
-	 * @see Walker::end_lvl()
-	 * @since 2.1.0
-	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @param int $depth Depth of category. Used for tab indentation.
-	 * @param array $args Will only append content if style argument value is 'list'.
-	 */
 	function end_lvl( &$output, $depth = 0, $args = array() ) {
 		if ( 'list' != $args['style'] )
 			return;
@@ -55,31 +21,12 @@ class shailan_CategoryWalker extends Walker {
 		$output .= "$indent</ul>\n";
 	}
 
-	/**
-	 * @see Walker::start_el()
-	 * @since 2.1.0
-	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @param object $category Category data object.
-	 * @param int $depth Depth of category in reference to parents.
-	 * @param array $args
-	 */
 	function start_el( &$output, $object, $depth = 0, $args = Array(), $current_object_id = 0 ) {
 		extract($args);
 
 		$cat_name = esc_attr( $category->name);
 		$cat_name = apply_filters( 'list_cats', $cat_name, $category );
 		$link = '<a href="' . get_category_link( $category->term_id ) . '" ';
-		/* 
-		
-		 @shailan: Disabled titles for dropdown menu.
-		
-		if ( $use_desc_for_title == 0 || empty($category->description) )
-			$link .= 'title="' . sprintf(__( 'View all posts filed under %s' ), $cat_name) . '"';
-		else
-			$link .= 'title="' . esc_attr( strip_tags( apply_filters( 'category_description', $category->description, $category ) ) ) . '"';
-			
-		*/
 		$link .= '><span>';
 		$link .= $cat_name . '</span></a>';
 
@@ -97,7 +44,6 @@ class shailan_CategoryWalker extends Walker {
 				$title = ' title="' . $feed . '"';
 				$alt = ' alt="' . $feed . '"';
 				$name = $feed;
-				//$link .= $title; // @shailan: removed title
 			}
 
 			$link .= '>';
@@ -105,7 +51,7 @@ class shailan_CategoryWalker extends Walker {
 			if ( empty($feed_image) )
 				$link .= $name;
 			else
-				$link .= "<img src='$feed_image'$alt" . ' />'; // @shailan: removed title
+				$link .= "<img src='$feed_image'$alt" . ' />';
 			$link .= '</a>';
 			if ( empty($feed_image) )
 				$link .= ')';
@@ -135,15 +81,6 @@ class shailan_CategoryWalker extends Walker {
 		}
 	}
 
-	/**
-	 * @see Walker::end_el()
-	 * @since 2.1.0
-	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @param object $page Not used.
-	 * @param int $depth Depth of category. Not used.
-	 * @param array $args Only uses 'list' for whether should append to output.
-	 */
 	function end_el( &$output, $object, $depth = 0, $args = Array() ) {
 		if ( 'list' != $args['style'] )
 			return;
